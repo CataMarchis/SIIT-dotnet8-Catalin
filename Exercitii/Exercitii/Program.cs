e3d3ff62d4987b5636d25cdd0a1115ef7fb7a7ca
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-22 02:58:46 +0300

    Alt exercitiu

--- Exercitii/Exercitii/Program.cs
+++ Exercitii/Exercitii/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Write a program that reads from the console three numbers of type int and prints their sum.
 
 Console.WriteLine("Write 3 numbers: ");
-Console.WriteLine("First number:");
+Console.WriteLine("First number: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Second number:");
-int b = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Third number:");int c = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Sum of these numbers is: " + (a + b + c));
+Console.WriteLine("Second number: ");
+int b = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Third number: ");int c = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Sum of these numbers is: " + (a + b + c));
---
+++ Exercitiu2/Exercitiu2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Write a program that reads five numbers from the console and prints the greatest of them.
+string[] numbers = { "First number: ", "Second number: ", "Third number: ", "Forth number: ", "Fifth number: " };
+int max;
+
+
+for(int i=0; i<numbers.Length; i++)
+{
+    
+    Console.Write(numbers[i]);
+    numbers[i] = Console.ReadLine();
+}
+max = Convert.ToInt32(numbers[0]);
+for (int i=0; i<numbers.Length;i++)
+{
+    if (Convert.ToInt32(numbers[i]) > max)
+    {  max = Convert.ToInt32(numbers[i]);}
+}
+Console.WriteLine("The greatest number is: " + max);
