c49dcd17391cb54c2a5af4bd499b13996f746f5d
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-22 03:23:56 +0300

    Al 3-lea exercitiu

---
+++ Exercitiu3/Exercitiu3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Write a program that reads from the console the radius "r" of a circle and prints its perimeter and area.
+
+
+double pi = Math.PI; 
+Console.Write("Circle radius is: ");
+double r = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Perimeter equals to: " + (2*pi*r));
+Console.WriteLine("Area of the circle equals to: " + (pi * r * r));
