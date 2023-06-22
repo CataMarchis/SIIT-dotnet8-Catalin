32738aadb82635c49770c1fda5bf10cf556719bb
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-22 03:14:18 +0300

    ...

--- Exercitii/Exercitii/Program.cs
+++ Exercitii/Exercitii/Program.cs
@@ -1,7 +1,14 @@
 ﻿// Write a program that reads from the console three numbers of type int and prints their sum.
 
 Console.WriteLine("Write 3 numbers: ");
+
 Console.WriteLine("First number: ");
 int a = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Second number: ");
-int b = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Third number: ");int c = Convert.ToInt32(Console.ReadLine());Console.WriteLine("Sum of these numbers is: " + (a + b + c));
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sum of these numbers is: " + (a + b + c));
