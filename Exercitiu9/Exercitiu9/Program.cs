f5184d55d2f833e547719aa7e949a653aab9a5d7
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-23 20:25:43 +0300

    Al 9lea exercitiu

---
+++ Exercitiu9/Exercitiu9/Program.cs
@@ -0,0 +1,25 @@
+﻿// Write a program that reads from the console a series of 5 integers and prints the smallest and largest of them.
+
+int[] integers = new int[5];
+int min;
+int max;
+
+Console.WriteLine(" Type 5 numbers: ");
+for (int i = 0; i < 5; i++)
+{
+    integers[i] = Convert.ToInt32(Console.ReadLine());
+}
+min = integers[0];
+max = integers[0];
+for (int i = 0; i < 5; i++)
+{
+    if (integers[i] < min)
+    {
+        min = integers[i];
+    }
+    else if (integers[i] > max)
+    {
+        max = integers[i];
+    }
+}
+Console.WriteLine("Smallest number is " + min + " and largest number is " + max);
