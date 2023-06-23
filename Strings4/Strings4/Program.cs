7d0d4c473ab4f291f4a60b612ae2cc299b2cd111
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-24 02:54:20 +0300

    All to upper if 2 are upper

---
+++ Strings4/Strings4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Write method to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters.
+using System;
+
+static string convertToUpper(string str)
+{
+    int nrOfUpper = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (char.IsUpper(str[i]))
+        {
+            nrOfUpper++;
+        }
+    }
+    if (nrOfUpper >= 2)
+    {
+        return str.ToUpper();
+    }
+    return str;
+}
+string myString = "Hellooo";
+Console.WriteLine(convertToUpper(myString));
