4a5e26f1066e89766b5216885d4ad47417b2e66e
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-23 19:12:19 +0300

    Al 6lea exercitiu, incomplet

---
+++ Exercitiu6/Exercitiu6/Program.cs
@@ -0,0 +1,26 @@
+﻿// Write a program that reads five integer numbers and prints their sum. If an invalid number is entered the program should prompt the user to enter another number(only once)
+
+
+int[] numbers = new int[5];
+int sum = 0;
+
+    Console.WriteLine("Write 5 integer numbers, press enter after each one: ");
+for (int i = 0; i < 5; i++)
+{
+    try
+    {
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+        sum = sum + numbers[i];
+    }
+
+        
+    catch
+    {
+    Console.WriteLine("Invalid number, enther another one: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    sum = sum + numbers[i];
+    }
+
+}
+    Console.WriteLine("Their sum is equal to: " + sum);
+
