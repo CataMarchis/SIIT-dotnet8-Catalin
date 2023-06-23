17726d7338e62e0c84cac0c055989560298952ab
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-23 19:49:26 +0300

    Al 7lea exercitiu

--- Exercitiu6/Exercitiu6/Program.cs
+++ Exercitiu6/Exercitiu6/Program.cs
@@ -4,7 +4,7 @@
 int[] numbers = new int[5];
 int sum = 0;
 
-    Console.WriteLine("Write 5 integer numbers, press enter after each one: ");
+Console.WriteLine("Write 5 integer numbers, press enter after each one: ");
 for (int i = 0; i < 5; i++)
 {
     try
@@ -13,14 +13,14 @@ for (int i = 0; i < 5; i++)
         sum = sum + numbers[i];
     }
 
-        
+
     catch
     {
-    Console.WriteLine("Invalid number, enther another one: ");
-    numbers[i] = Convert.ToInt32(Console.ReadLine());
-    sum = sum + numbers[i];
+        Console.WriteLine("Invalid number, enter another one: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+        sum = sum + numbers[i];
     }
 
 }
-    Console.WriteLine("Their sum is equal to: " + sum);
+Console.WriteLine("Their sum is equal to: " + sum);
 
---
+++ Exercitiu7/Exercitiu7/Program.cs
@@ -0,0 +1,11 @@
+﻿// Write a program that prints on the console the numbers from 1 to N. The number N should be read from the standard input.
+
+
+Console.Write("Type a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("All the numbers from 1 to " + n + " are: ");
+
+for(int i = 0; i <= n; i++)
+{
+    Console.WriteLine(i);
+}
