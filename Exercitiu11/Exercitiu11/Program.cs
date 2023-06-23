725061002e6a86da08e8b4bd18369fd6a9df125c
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-23 21:20:14 +0300

    Al 11lea exercitiu

---
+++ Exercitiu11/Exercitiu11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Write a program that converts a given number from decimal to binary notation (numeral system).
+
+string result = "";
+Console.Write(" Give a number to convert to binary: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number >= 1)
+{
+    int remainder = number % 2;
+    result = Convert.ToInt32(remainder) + result;
+    number = number / 2;
+}
+
+Console.WriteLine(" Binary version of the number is: " + result);
+
+
+
+
+
