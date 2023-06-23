db63a1afd328047339ccedb6e6263768c2d581ac
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-24 00:25:39 +0300

    Remove odd from string

---
+++ Strings1/Strings1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Write a method that to remove the characters which have odd index values of a given string.
+
+string str = "Hello, how are you?";
+
+static string removeOdd(string strin)
+{
+    string strin2 = "";
+    for(int i = 0; i < strin.Length; i++)
+    {
+        if (i % 2 == 0)
+            strin2 = strin2 + strin[i];
+    }
+    return strin2;
+}
+
+
+Console.WriteLine(removeOdd(str));
