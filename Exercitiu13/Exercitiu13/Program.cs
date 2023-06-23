137cba0210bd9100277696cc388624ade448890a
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-24 00:57:03 +0300

    Upper and lower

--- Exercitiu13/Exercitiu13/Program.cs
+++ Exercitiu13/Exercitiu13/Program.cs
@@ -1,11 +1,9 @@
-﻿for (int i = 1; i <= 50; i++)
+﻿int n = 5;
+int j =25;
+for(int i = 0; i < n;i++)
 {
-    if (i % 3 == 0 && i % 5 == 0)
-        Console.WriteLine("BuzzFizz");
-    else if (i % 3 == 0)
-        Console.WriteLine("Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine("Buzz");
-    else 
-        Console.WriteLine(i);
+    j = i;
+    Console.WriteLine(j);
 }
+
+Console.WriteLine(j);
---
+++ Strings2/Strings2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Write a method that takes input from the user and displays that input back in upper and lower cases.
+
+
+static void display()
+{
+    Console.Write("Write a string: ");
+    string str = Console.ReadLine();
+    string upper = str.ToUpper();
+    string lower = str.ToLower();
+    Console.WriteLine("Given string to upper case: " + upper);
+    Console.WriteLine("Given string to lower case: " + lower);
+}
+
+display();
