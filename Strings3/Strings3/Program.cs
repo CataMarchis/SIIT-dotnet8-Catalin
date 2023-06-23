cf6d89ceecf277d4263399022085501c561ec2e9
Marchis Catalin <marchiscatalin@yahoo.com>
2023-06-24 02:15:39 +0300

    Reversed string

---
+++ Strings3/Strings3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Write a method that reverses a string if it's length is a multiple of 4
+
+static string reverse(string str)
+{
+    char[] stringArray = str.ToCharArray();
+    if (stringArray.Length % 4 == 0)
+        Array.Reverse(stringArray);
+    string reversedStr = new(stringArray);
+    return reversedStr;
+}
+Console.WriteLine(reverse("Hellooow"));
+
